33f31067f6bf743a410c06f3ec558d60a7988b30
SENA <SENA@10.12.6.98>
2017-04-26 12:07:57 -0500

    Version juan

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -15,7 +15,7 @@ public class Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola juan!");
     }
     
 }
